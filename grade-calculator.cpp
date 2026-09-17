c3885fc76256e1a6d8432f88fafc80f92806b9f3
Diwakar-cl <diwakar64gamal@gmail.com>
2026-09-17 14:29:59 +0545

    Refactor grade calculator with functions for GPA and grading
    Refactored GPA calculation and grade printing into separate functions. Updated main function to handle multiple students' grades.

--- grade-calculator.cpp
+++ grade-calculator.cpp
@@ -1,38 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main() {
 
-    int students;
-    std::cout <<"How many students: " << std::endl;
-    std::cin >>students;
+double GPA(double a, double b, double c) {
+    return (a+b+c)/3;
+}
 
-    for(int i=1; i<=students; i=i+1) {
-        std::cout <<" student " <<i<<std::endl;
-    
-
-    double m1, m2, m3;
-    std::cout <<"Marks for math:" <<std::endl;
-    std::cin >>m1;
-    std::cout <<"Marks for science: " <<std::endl;
-    std::cin >>m2;
-    std::cout <<"Marks for English: " <<std::endl;
-    std::cin >>m3;
-    
+void printgrade(double AVG) {
 
 
-
-    double GPA =(m1+m2+m3)/3;
-    std::cout <<"Your GPA: " <<GPA <<std::endl;
-
-    if(GPA >=90) {
+   
+    if(AVG >=90) {
         std::cout <<"Your grade:A" <<std::endl;
     }
-    else if(GPA >=60) {
+    else if(AVG >=60) {
         std::cout <<"Your grade :B" <<std::endl;
     }
 
-    else if(GPA >=40) {
+    else if(AVG >=40) {
         std::cout <<"Your GPA: C" <<std::endl;
     }
 
@@ -40,6 +25,24 @@ int main() {
     else{
         std::cout <<"You failed" <<std::endl;
     }
+}
+
+
+int main() {
+
+    int students;
+    std::cout <<"How many students: " << std::endl;
+    std::cin >>students;
+
+    for(int i=1; i<=students; i=i+1) {
+        double m1,m2,m3;
+        std::cin >>m1 >>m2 >>m3;
+
+        double AVG = GPA(m1, m2, m3);
+        std::cout <<"Your GPA: " <<AVG <<std::endl;
+        printgrade(AVG);
+
+    
 }
     return 0;
 
